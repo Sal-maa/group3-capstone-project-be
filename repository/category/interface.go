7eda63dc1e40e9420653d4442e5206c94f47de4a
zakariyahya <yazakkars@gmail.com>
2022-02-20 21:10:54 +0700

    create asset

--- repository/category/interface.go
+++ repository/category/interface.go
@@ -5,9 +5,5 @@ import (
 )
 
 type Category interface {
-	// Create(assetData _entity.Asset) (createAsset _entity.Asset, code int, err error)
-	// GetById(id int) (asset _entity.Asset, code int, err error)
 	GetAll() (category []_entity.Category, code int, err error)
-	// Update(assetData _entity.Asset) (updateAsset _entity.Asset, code int, err error)
-	// Delete(id int) (code int, err error)
 }
