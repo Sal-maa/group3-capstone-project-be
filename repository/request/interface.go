64982f54f3dd94e59c24f3e2fcf959aa94ae2eb7
SALMA salma <salmaa.matematika@gmail.com>
2022-02-21 17:03:24 +0700

    add repo check status

--- repository/request/interface.go
+++ repository/request/interface.go
@@ -3,6 +3,7 @@ package request
 import "capstone/be/entity"
 
 type Request interface {
+	CheckMaintenance(AssetId int) (asset entity.Asset, err error)
 	Borrow(reqData entity.Borrow) (entity.Borrow, error)
 	Procure(reqData entity.Procure) (entity.Procure, error)
 }
