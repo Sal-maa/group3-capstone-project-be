d13b91733e8e3abe16f4878fb4699b55a26ec2a4
SALMA salma <salmaa.matematika@gmail.com>
2022-02-21 21:30:46 +0700

    fix with db prepare

--- repository/request/request.go
+++ repository/request/request.go
@@ -2,6 +2,7 @@ package request
 
 import (
 	"capstone/be/entity"
+	_entity "capstone/be/entity"
 	"database/sql"
 )
 
@@ -40,12 +41,27 @@ func (rr *RequestRepository) CheckMaintenance(AssetId int) (asset entity.Asset, 
 	return asset, nil
 }
 
-func (rr *RequestRepository) Borrow(reqData entity.Borrow) (entity.Borrow, error) {
-	_, err := rr.db.Exec("INSERT INTO `borrow/return_requests` (updated_at, user_id, asset_id, activity, request_time, return_time, status, description) VALUES(?,?,?,?,?,?,?,?)", reqData.UpdatedAt, reqData.User.Id, reqData.Asset.Id, reqData.Activity, reqData.RequestTime, reqData.ReturnTime, reqData.Status, reqData.Description)
+func (rr *RequestRepository) Borrow(reqData _entity.Borrow) (_entity.Borrow, error) {
+	statement, err := rr.db.Prepare("INSERT INTO `borrow/return_requests` (updated_at, user_id, asset_id, activity, request_time, return_time, status, description) VALUES(?,?,?,?,?,?,?,?)")
+	if err != nil {
+		return reqData, err
+	}
+
+	defer statement.Close()
+
+	_, err = statement.Exec(reqData.UpdatedAt, reqData.User.Id, reqData.Asset.Id, reqData.Activity, reqData.RequestTime, reqData.ReturnTime, reqData.Status, reqData.Description)
+
 	return reqData, err
 }
 
 func (rr *RequestRepository) Procure(reqData entity.Procure) (entity.Procure, error) {
-	_, err := rr.db.Exec("INSERT INTO `procurement_requests` (updated_at, user_id, category_id, image, activity, request_time, status, description) VALUES(?,?,?,?,?,?,?,?)", reqData.UpdatedAt, reqData.CategoryId, reqData.Image, reqData.Activity, reqData.RequestTime, reqData.Status, reqData.Description)
+	statement, err := rr.db.Prepare("INSERT INTO `procurement_requests` (updated_at, user_id, category_id, image, activity, request_time, status, description) VALUES(?,?,?,?,?,?,?,?)")
+	if err != nil {
+		return reqData, err
+	}
+
+	defer statement.Close()
+
+	_, err = statement.Exec(reqData.UpdatedAt, reqData.CategoryId, reqData.Image, reqData.Activity, reqData.RequestTime, reqData.Status, reqData.Description)
 	return reqData, err
 }
