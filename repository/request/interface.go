a05527965c293d3e2f05e2cf6cac3c3bd27cc557
SALMA salma <salmaa.matematika@gmail.com>
2022-02-21 16:46:41 +0700

    add procurement repo

--- repository/request/interface.go
+++ repository/request/interface.go
@@ -4,4 +4,5 @@ import "capstone/be/entity"
 
 type Request interface {
 	Borrow(reqData entity.Borrow) (entity.Borrow, error)
+	Procure(reqData entity.Procure) (entity.Procure, error)
 }
--- repository/request/request.go
+++ repository/request/request.go
@@ -14,6 +14,11 @@ func New(db *sql.DB) *RequestRepository {
 }
 
 func (rr *RequestRepository) Borrow(reqData entity.Borrow) (entity.Borrow, error) {
-	_, err := rr.db.Exec("INSERT INTO `borrow/return_requests` (created_at, updated_at, user_id, asset_id, activity, request_time, return_time, status, description) VALUES(?,?,?,?,?,?,?,?,?)", reqData.CreatedAt, reqData.UpdatedAt, reqData.User.Id, reqData.Asset.Id, reqData.Activity, reqData.RequestTime, reqData.ReturnTime, reqData.Status, reqData.Description)
+	_, err := rr.db.Exec("INSERT INTO `borrow/return_requests` (updated_at, user_id, asset_id, activity, request_time, return_time, status, description) VALUES(?,?,?,?,?,?,?,?)", reqData.UpdatedAt, reqData.User.Id, reqData.Asset.Id, reqData.Activity, reqData.RequestTime, reqData.ReturnTime, reqData.Status, reqData.Description)
+	return reqData, err
+}
+
+func (rr *RequestRepository) Procure(reqData entity.Procure) (entity.Procure, error) {
+	_, err := rr.db.Exec("INSERT INTO `procurement_requests` (updated_at, user_id, category_id, image, activity, request_time, status, description) VALUES(?,?,?,?,?,?,?,?)", reqData.UpdatedAt, reqData.CategoryId, reqData.Image, reqData.Activity, reqData.RequestTime, reqData.Status, reqData.Description)
 	return reqData, err
 }
