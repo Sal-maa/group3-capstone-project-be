58af5dde040ac08d85de1f33bf3c8be73ddd90c3
SALMA salma <salmaa.matematika@gmail.com>
2022-02-21 14:20:36 +0700

    add request repo on main.go

--- app/main.go
+++ app/main.go
@@ -2,13 +2,17 @@ package main
 
 import (
 	_config "capstone/be/config"
+	_requestController "capstone/be/delivery/controller/request"
 	_userController "capstone/be/delivery/controller/user"
 	_midware "capstone/be/delivery/middleware"
 	_router "capstone/be/delivery/router"
+	_requestRepo "capstone/be/repository/request"
 	_userRepo "capstone/be/repository/user"
 	_util "capstone/be/util"
 	"fmt"
 	"log"
+
+	_ "github.com/go-sql-driver/mysql"
 
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
@@ -30,14 +34,16 @@ func main() {
 	defer db.Close()
 
 	userRepo := _userRepo.New(db)
+	requestRepo := _requestRepo.New(db)
 
 	userController := _userController.New(userRepo)
+	requestController := _requestController.New(requestRepo)
 
 	e := echo.New()
 
 	e.Pre(middleware.RemoveTrailingSlash(), middleware.CORS(), _midware.CustomLogger())
 
-	_router.RegisterPath(e, userController)
+	_router.RegisterPath(e, userController, requestController)
 
 	address := fmt.Sprintf(":%d", config.Port)
 	e.Logger.Fatal(e.Start(address))
