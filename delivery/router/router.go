6c5635083797d3e29029d64b11760c58ea6eebdf
bagusbpg <bagusbpg@gmail.com>
2022-02-21 09:37:40 +0700

    seed assets

--- delivery/router/router.go
+++ delivery/router/router.go
@@ -23,7 +23,7 @@ func RegisterPath(
 	e.POST("/login", userController.Login())
 
 	// User
-	e.GET("/users", userController.GetAll())
-	e.GET("/users/:id", userController.GetById())
+	e.GET("/users", userController.GetAll(), _midware.JWTMiddleWare())
+	e.GET("/users/:id", userController.GetById(), _midware.JWTMiddleWare())
 	e.PUT("/users/:id", userController.Update(), _midware.JWTMiddleWare())
 }
