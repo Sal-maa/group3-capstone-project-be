e34f0b0633fb9702d7b049e6c217563658d57b79
bagusbpg <bagusbpg@gmail.com>
2022-02-22 18:29:08 +0700

    add get active request by id

--- entity/activity.go
+++ entity/activity.go
@@ -2,10 +2,21 @@ package entity
 
 import "time"
 
+type Activity struct {
+	Id          int       `json:"id" form:"id"`
+	Category    string    `json:"category" form:"category"`
+	AssetName   string    `json:"asset_name" form:"asset_name"`
+	AssetImage  string    `json:"asset_image" form:"asset_image"`
+	UserName    string    `json:"user_name" form:"user_name"`
+	RequestDate time.Time `json:"request_date" form:"request_date"`
+	Status      string    `json:"status" form:"status"`
+	Description string    `json:"note" form:"note"`
+}
+
 type ActivitySimplified struct {
 	Id          int       `json:"id" form:"id"`
-	Image       string    `json:"image" form:"image"`
-	Name        string    `json:"name" form:"name"`
+	AssetImage  string    `json:"asset_image" form:"asset_image"`
+	AssetName   string    `json:"asset_name" form:"asset_name"`
 	Status      string    `json:"status" form:"status"`
 	RequestDate time.Time `json:"request_date" form:"request_date"`
 }
--- repository/activity/activity.go
+++ repository/activity/activity.go
@@ -2,6 +2,9 @@ package activity
 
 import (
 	_entity "capstone/be/entity"
+	"errors"
+	"fmt"
+	"log"
 
 	"database/sql"
 	"net/http"
@@ -15,7 +18,53 @@ func New(db *sql.DB) *ActivityRepository {
 	return &ActivityRepository{db: db}
 }
 
-func (ar ActivityRepository) GetAll() (activities []_entity.ActivitySimplified, code int, err error) {
+func (ar ActivityRepository) GetAllRequestOfUser(user_id int) (activities []_entity.ActivitySimplified, code int, err error) {
+	stmt, err := ar.db.Prepare(`
+		SELECT b.id, a.name, a.image, b.status, b.request_time
+		FROM borrowORreturn_requests b
+		JOIN assets a
+		ON b.asset_id = a.id
+		WHERE b.deleted_at IS NULL
+		  AND b.activity <> "Return"
+		  AND b.status <> "Approved by Admin"
+		  AND b.user_id = ?
+	`)
+
+	if err != nil {
+		log.Println(err)
+		code, err = http.StatusInternalServerError, errors.New("internal server error")
+		return activities, code, err
+	}
+
+	defer stmt.Close()
+
+	res, err := stmt.Query()
+
+	if err != nil {
+		log.Println(err)
+		code, err = http.StatusInternalServerError, errors.New("internal server error")
+		return activities, code, err
+	}
+
+	defer res.Close()
+
+	for res.Next() {
+		activity := _entity.ActivitySimplified{}
+
+		if err := res.Scan(&activity.Id, &activity.AssetName, &activity.AssetImage, &activity.Status, &activity.RequestDate); err != nil {
+			log.Println(err)
+			code, err = http.StatusInternalServerError, errors.New("internal server error")
+			return activities, code, err
+		}
+
+		activity.AssetImage = fmt.Sprintf("https://capstone-group3.s3.ap-southeast-1.amazonaws.com/%s", activity.AssetImage)
+
+		activities = append(activities, activity)
+	}
 
 	return activities, http.StatusOK, nil
+}
+
+func (ar ActivityRepository) GetByRequestId(request_id int) (activity _entity.Activity, code int, err error) {
+	return activity, http.StatusOK, nil
 }
