8e016de967d6e38b9e7ef316f1707d0873ff4494
SALMA salma <salmaa.matematika@gmail.com>
2022-02-22 15:22:44 +0700

    add repo get division id, get asset id and get borrow by id

--- repository/request/interface.go
+++ repository/request/interface.go
@@ -5,10 +5,13 @@ import (
 )
 
 type Request interface {
+	GetAssetId(newReq _entity.CreateBorrow) (id int, err error)
 	CheckMaintenance(reqData _entity.Borrow) (asset _entity.Asset, err error)
 	Borrow(reqData _entity.Borrow) (_entity.Borrow, error)
 	GetCategoryId(newReq _entity.CreateProcure) (id int, err error)
 	Procure(reqData _entity.Procure) (_entity.Procure, error)
+	GetUserDivision(id int) (divId int, err error)
+	GetBorrowById(id int) (req _entity.Borrow, err error)
 	UpdateBorrow(reqData _entity.Borrow) (_entity.Borrow, error)
 	UpdateProcure(reqData _entity.Procure) (_entity.Procure, error)
 }
--- repository/request/request.go
+++ repository/request/request.go
@@ -14,9 +14,41 @@ func New(db *sql.DB) *RequestRepository {
 	return &RequestRepository{db: db}
 }
 
+func (rr *RequestRepository) GetAssetId(newReq _entity.CreateBorrow) (id int, err error) {
+	stmt, err := rr.db.Prepare(`
+		SELECT 
+			id 
+		FROM 
+			assets 
+		WHERE status = "Available" AND deleted_at IS NULL AND name = ? 
+		ORDER BY id ASC LIMIT 1
+	`)
+
+	if err != nil {
+		return id, err
+	}
+
+	defer stmt.Close()
+
+	res, err := stmt.Query(newReq.AssetName)
+
+	if err != nil {
+		return id, err
+	}
+
+	defer res.Close()
+
+	if res.Next() {
+		if err := res.Scan(&id); err != nil {
+			return id, err
+		}
+	}
+	return id, nil
+}
+
 func (rr *RequestRepository) CheckMaintenance(reqData _entity.Borrow) (asset _entity.Asset, err error) {
 	stmt, err := rr.db.Prepare(`
-		SELECT status FROM assets WHERE deleted_at IS NULL AND id = ?
+		SELECT status FROM assets WHERE deleted_at IS NULL AND name = ?
 	`)
 
 	if err != nil {
@@ -25,7 +57,7 @@ func (rr *RequestRepository) CheckMaintenance(reqData _entity.Borrow) (asset _en
 
 	defer stmt.Close()
 
-	res, err := stmt.Query(reqData.Asset.Id)
+	res, err := stmt.Query(reqData.Asset.Name)
 
 	if err != nil {
 		return asset, err
@@ -98,6 +130,70 @@ func (rr *RequestRepository) Procure(reqData _entity.Procure) (_entity.Procure, 
 
 	_, err = statement.Exec(reqData.UpdatedAt, reqData.Category, reqData.Image, reqData.Activity, reqData.RequestTime, reqData.Status, reqData.Description)
 	return reqData, err
+}
+
+func (rr *RequestRepository) GetUserDivision(id int) (divId int, err error) {
+	stmt, err := rr.db.Prepare(`
+		SELECT 
+			division_id
+		FROM 
+			users 
+		WHERE deleted_at IS NULL AND id = ? 
+		ORDER BY id ASC LIMIT 1
+	`)
+
+	if err != nil {
+		return divId, err
+	}
+
+	defer stmt.Close()
+
+	res, err := stmt.Query(id)
+
+	if err != nil {
+		return divId, err
+	}
+
+	defer res.Close()
+
+	if res.Next() {
+		if err := res.Scan(&divId); err != nil {
+			return divId, err
+		}
+	}
+	return divId, nil
+}
+
+func (rr *RequestRepository) GetBorrowById(id int) (req _entity.Borrow, err error) {
+	stmt, err := rr.db.Prepare(`
+		SELECT 
+			id, user_id, asset_id, activity, request_time, return_time, status, description 
+		FROM 
+			borrowORreturn_requests 
+		WHERE status = "Waiting Approval" AND deleted_at IS NULL AND id = ? 
+		ORDER BY id ASC LIMIT 1
+	`)
+
+	if err != nil {
+		return req, err
+	}
+
+	defer stmt.Close()
+
+	res, err := stmt.Query(id)
+
+	if err != nil {
+		return req, err
+	}
+
+	defer res.Close()
+
+	if res.Next() {
+		if err := res.Scan(&req.Id, &req.User.Id, &req.Asset.Id, &req.Activity, &req.RequestTime, &req.ReturnTime, &req.Status, &req.Description); err != nil {
+			return req, err
+		}
+	}
+	return req, nil
 }
 
 func (rr *RequestRepository) UpdateBorrow(reqData _entity.Borrow) (_entity.Borrow, error) {
