bfe373e1758a792b02294785fc068e4a063adb81
Your Name <you@example.com>
2022-02-23 10:26:30 +0700

    pull

--- delivery/router/router.go
+++ delivery/router/router.go
@@ -37,7 +37,5 @@ func RegisterPath(
 	e.POST("/assets", assetController.Create(), _midware.JWTMiddleWare())
 	e.GET("/assets", assetController.GetAll())
 	e.GET("/assets/:category", assetController.GetAssetByCategory())
-
-	// e.GET("/assets/:id", assetController.GetById())
 	e.PUT("/assets/:id", assetController.Update(), _midware.JWTMiddleWare())
 }
