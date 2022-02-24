5686272bab343eb999d6a067308e48890d9638b4
SALMA salma <salmaa.matematika@gmail.com>
2022-02-24 09:20:31 +0700

    add request respon entity

--- entity/request.go
+++ entity/request.go
@@ -11,7 +11,6 @@ type Borrow struct {
 	ReturnTime  time.Time `json:"return_time" form:"return_time"`
 	Status      string    `json:"status" form:"status"`
 	Description string    `json:"description" form:"description"`
-	CreatedAt   time.Time `json:"created_at" form:"created_at"`
 	UpdatedAt   time.Time `json:"updated_at" form:"updated_at"`
 	DeletedAt   time.Time `json:"deleted_at" form:"deleted_at"`
 }
@@ -37,7 +36,6 @@ type Procure struct {
 	RequestTime time.Time `json:"request_time" form:"request_time"`
 	Status      string    `json:"status" form:"status"`
 	Description string    `json:"description" form:"description"`
-	CreatedAt   time.Time `json:"created_at" form:"created_at"`
 	UpdatedAt   time.Time `json:"updated_at" form:"updated_at"`
 	DeletedAt   time.Time `json:"deleted_at" form:"deleted_at"`
 }
@@ -52,4 +50,17 @@ type CreateProcure struct {
 
 type UpdateProcure struct {
 	Status string `json:"status" form:"status"`
+}
+
+type RequestResponse struct {
+	Id          int `json:"id" form:"id"`
+	User        User
+	Asset       Asset
+	Activity    string    `json:"activity" form:"activity"`
+	RequestTime time.Time `json:"request_time" form:"request_time"`
+	ReturnTime  time.Time `json:"return_time" form:"return_time"`
+	Status      string    `json:"status" form:"status"`
+	Description string    `json:"description" form:"description"`
+	UpdatedAt   time.Time `json:"updated_at" form:"updated_at"`
+	DeletedAt   time.Time `json:"deleted_at" form:"deleted_at"`
 }
