0a277e898c30d6d32cca0becc7f3b288f85829ff
SALMA salma <salmaa.matematika@gmail.com>
2022-02-24 14:55:37 +0700

    add filter category, status and date

--- delivery/controller/admin/admin.go
+++ delivery/controller/admin/admin.go
@@ -4,6 +4,7 @@ import (
 	_common "capstone/be/delivery/common"
 	"log"
 	"strconv"
+	"strings"
 
 	// _helper "capstone/be/delivery/helper"
 	_midware "capstone/be/delivery/middleware"
@@ -33,6 +34,10 @@ func (ac AdminController) HomePageGetAll() echo.HandlerFunc {
 
 		// filter by page number
 		p := c.QueryParam("p")
+		// default value for page
+		if p == "" {
+			p = "1"
+		}
 
 		limit, err := strconv.Atoi(p)
 
@@ -48,7 +53,48 @@ func (ac AdminController) HomePageGetAll() echo.HandlerFunc {
 		if err != nil {
 			return c.JSON(http.StatusBadRequest, _common.NoDataResponse(http.StatusBadRequest, "Error parsing record of page"))
 		}
-		requests, err := ac.repository.GetAllNewRequest(limit, offset)
+
+		// filter by status
+		status := c.QueryParam("s")
+
+		// default value for status
+		if status == "" {
+			status = "all"
+		}
+
+		// to prevent sql injection
+		if strings.ContainsAny(status, ";") {
+			return c.JSON(http.StatusBadRequest, _common.NoDataResponse(http.StatusBadRequest, "Bad request"))
+		}
+
+		allstatus := map[string]int{"all": 1, "Waiting Approval": 1, "Approved": 1, "Rejected": 1, "Request to Return": 1}
+
+		if _, exist := allstatus[status]; !exist {
+			return c.JSON(http.StatusBadRequest, _common.NoDataResponse(http.StatusBadRequest, "Bad request"))
+		}
+		// filter by date
+		date := c.QueryParam("d")
+
+		// filter by category
+		category := c.QueryParam("category")
+
+		// to prevent sql injection
+		if strings.ContainsAny(category, ";") {
+			return c.JSON(http.StatusBadRequest, _common.NoDataResponse(http.StatusBadRequest, "Bad request"))
+		}
+
+		// default value for category
+		if category == "" {
+			category = "all"
+		}
+
+		categories := map[string]int{"all": 1, "Computer": 1, "Computer Accessories": 1, "Networking": 1, "UPS": 1, "Printer and Scanner": 1, "Electronics": 1, "Others": 1}
+
+		if _, exist := categories[category]; !exist {
+			return c.JSON(http.StatusBadRequest, _common.NoDataResponse(http.StatusBadRequest, "Bad request"))
+		}
+
+		requests, err := ac.repository.GetAllNewRequest(limit, offset, status, date)
 		if err != nil {
 			log.Println(err)
 			return c.JSON(http.StatusInternalServerError, _common.NoDataResponse(http.StatusInternalServerError, "Failed to read data"))
