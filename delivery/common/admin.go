f06fcb4fb96ee1cb67f5d29f649dac274829f5db
SALMA salma <salmaa.matematika@gmail.com>
2022-03-02 12:37:13 +0700

    fix formatting response

--- delivery/common/admin.go
+++ delivery/common/admin.go
@@ -5,19 +5,21 @@ import (
 	"net/http"
 )
 
-func GetAllRequestResponse(requests []_entity.RequestResponse) map[string]interface{} {
+func GetAllRequestResponse(requests []_entity.RequestResponse, total int) map[string]interface{} {
 	return map[string]interface{}{
-		"code":    http.StatusOK,
-		"message": "success get all requests",
-		"data":    requests,
+		"code":         http.StatusOK,
+		"message":      "success get all requests",
+		"data":         requests,
+		"total_record": total,
 	}
 }
 
-func GetAllProcureRequestResponse(requests []_entity.Procure) map[string]interface{} {
+func GetAllProcureRequestResponse(requests []_entity.Procure, total int) map[string]interface{} {
 	return map[string]interface{}{
-		"code":    http.StatusOK,
-		"message": "success get all requests",
-		"data":    requests,
+		"code":         http.StatusOK,
+		"message":      "success get all requests",
+		"data":         requests,
+		"total_record": total,
 	}
 }
 
